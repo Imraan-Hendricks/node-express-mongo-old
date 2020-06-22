6cba6a5d23e2ca5a950ce5361ae34beb71a5b388
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-06-22 17:33:02 +0200

    set static directories

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.listen(5000, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${5000}`);
