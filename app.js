d7de695340ba8c5d546900846f6ba7f001a8e358
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-06-22 16:01:13 +0200

    added npm scripts

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const app = express();
 app.use(express.json());
 
 app.listen(5000, () => {
-  console.log(`Server started on port ${5000}`);
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${5000}`);
 });
