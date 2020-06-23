066558e0f74c7590a1b1404008dc0804eb971df6
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-06-23 14:55:16 +0200

    added shutdown service

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('./config/db');
 const dotenv = require('dotenv');
 const path = require('path');
+const { shutdown } = require('./services/services');
 
 dotenv.config({ path: './config/.env' });
 
@@ -14,8 +15,13 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+app.use(shutdown.handleRequests());
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
+
+shutdown.onInterrupt(server, db);
+shutdown.onTerminate(server, db);
---
+++ services/shutdown.js
@@ -0,0 +1,35 @@
+var state = false;
+
+const cleanup = (server, db) => {
+  state = true;
+  console.log('Closing http server');
+  server.close(() => {
+    console.log('Http server closed');
+    db.closeConnections().then(() => {
+      console.log('Shutting down');
+      process.exit(0);
+    });
+  });
+
+  setTimeout(() => {
+    console.error('Could not close connections in time, forcing shut down');
+    process.exit(1);
+  }, 30 * 1000);
+};
+
+exports.handleRequests = () => {
+  return (req, res, next) => {
+    if (!state) return next();
+
+    res.setHeader('Connection', 'close');
+    res.status(503).send('Server is in the process of restarting');
+  };
+};
+
+exports.onInterrupt = (server, db) => {
+  process.on('SIGINT', () => cleanup(server, db));
+};
+
+exports.onTerminate = (server, db) => {
+  process.on('SIGTERM', () => cleanup(server, db));
+};
