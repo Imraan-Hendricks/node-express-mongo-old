411f4a87c0dfcd463f522aa8df81c593a8dcfc03
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-07-01 20:55:20 +0200

    added passport serialization

--- services/session.js
+++ services/session.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const session = require('express-session');
+const User = require('../models/User');
 
 module.exports = (app) => {
   app.use(
@@ -12,4 +13,12 @@ module.exports = (app) => {
 
   app.use(passport.initialize());
   app.use(passport.session());
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => done(err, user));
+  });
 };
