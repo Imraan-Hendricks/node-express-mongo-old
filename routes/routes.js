9fa3d463ce1e2e99596f7ef87d34f9db79407ad3
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-07-04 02:34:21 +0200

    updated catch route to handle dev and prod mode

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,17 @@ const path = require('path');
 module.exports = (app) => {
   app.use('/api', require('./api/api'));
   app.use('/backOffice', require('./back-office/back-office'));
+
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+    let client;
+
+    if (process.env.NODE_ENV !== 'production') {
+      client = 'http://localhost:3000';
+      const url = req.originalUrl;
+      return res.redirect(client + url);
+    }
+
+    client = path.join(__dirname, '../../client/build/index.html');
+    res.sendFile(client);
   });
 };
